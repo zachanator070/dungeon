950116d980a96300734e78272010ea6e15248693
Zach Johnson <zjohnson@adobe.com>
2024-08-02 12:50:29 -0600

    updated styles

--- console.js
+++ console.js
@@ -1,164 +1,160 @@
-
-// global variables
-var lines = ['You find yourself in a dark dank dungeon.','Typing \'help\' would probably help.','>'];
-var maxCharsPerLine = 50;
-var maxLinesPerScreen = 14;
+const lines = ['You find yourself in a dark dank dungeon.', 'Typing \'help\' would probably help.', '>'];
+const maxCharsPerLine = 50;
+const maxLinesPerScreen = 12;
 
 // used to determine if we will accept more commands
-var playing = true;
+let playing = true;
 
-function addChar(event){
+function addChar(event) {
 
-  if(!playing){
-    return;
-  }
-
-  var code = event.which || event.keyCode;
-
-  var key = String.fromCharCode(code);
-
-  var lastLine = lines[lines.length-1];
-
-  // if any key was pressed, and the cursor was showing, delete it
-  if(lastLine[lastLine.length-1] == "|"){
-    lines[lines.length-1] = lastLine.substring(0, lastLine.length - 1);
-    lastLine = lines[lines.length-1];
-  }
-
-  // if enter was pressed then process command
-  if(key == "\r"){
-
-    if(lastLine[0] == ">"){
-      lastLine = lastLine.substr(1,lastLine.length - 1)
+    if (!playing) {
+        return;
     }
 
-    runCommand(lastLine);
+    let code = event.which || event.code;
 
-    if(playing){
-      addNewLine(">");
+    let key = String.fromCharCode(code);
+
+    let lastLine = lines[lines.length - 1];
+
+    // if any key was pressed, and the cursor was showing, delete it
+    if (lastLine[lastLine.length - 1] === "|") {
+        lines[lines.length - 1] = lastLine.substring(0, lastLine.length - 1);
+        lastLine = lines[lines.length - 1];
     }
 
-  }
-  // if backspace was pressed, then delete a character
-  else if(code == 8){
-    if(lastLine.length > 1){
-      lines[lines.length-1] = lastLine.substring(0, lastLine.length - 1);
+    // if enter was pressed then process command
+    if (key === "\r") {
+
+        if (lastLine[0] === ">") {
+            lastLine = lastLine.substring(1, lastLine.length)
+        }
+
+        runCommand(lastLine);
+
+        if (playing) {
+            addNewLine(">");
+        }
+
     }
-  }
-  // if we pressed the shift key, do not add a character
-  else if(code == 16){
-    return;
-  }
-
-  // else we just need to add a new character
-  else{
-
-    // if shift key was not pressed we need to make the letter lowercase
-    if(!event.shiftKey && code >= 65 && code <= 90){
-      code += 32;
-      key = String.fromCharCode(code);
+    // if backspace was pressed, then delete a character
+    else if (code === 8) {
+        if (lastLine.length > 1) {
+            lines[lines.length - 1] = lastLine.substring(0, lastLine.length - 1);
+        }
+    }
+    // if we pressed the shift key, do not add a character
+    else if (code === 16) {
+        return;
     }
 
-    // if we went over the max charactters per line, add a new line
-    if(lastLine.length == maxCharsPerLine){
-      addNewLine("");
+    // else we just need to add a new character
+    else {
+
+        // if shift key was not pressed we need to make the letter lowercase
+        if (!event.shiftKey && code >= 65 && code <= 90) {
+            code += 32;
+            key = String.fromCharCode(code);
+        }
+
+        // if we went over the max characters per line, add a new line
+        if (lastLine.length === maxCharsPerLine) {
+            addNewLine("");
+        }
+        lines[lines.length - 1] += key;
+
     }
-    lines[lines.length-1] += key;
 
-  }
-
-  // update with the new info
-  updateConsole();
+    // update with the new info
+    updateConsole();
 
 }
 
 // adds new lines to the lines array
-function addNewLine(line){
+function addNewLine(line) {
 
-  var moreLines = line.split("\n");
+    const moreLines = line.split("\n");
 
-  if(moreLines.length >1){
-    moreLines.forEach(function(line){
+    if (moreLines.length > 1) {
+        moreLines.forEach(function (line) {
 
-      addNewLine(line);
+            addNewLine(line);
 
-    });
-    return;
-  }
-
-  if(line.length > maxCharsPerLine){
-    var words = line.split(" ");
-    var tempLine = "";
-
-    words.forEach(function(word){
-      if((tempLine+word).length + 1 < maxCharsPerLine){
-        tempLine += word + " ";
-      }
-      else{
-        addNewLine(tempLine);
-        tempLine = word + " ";
-      }
-    });
-
-    addNewLine(tempLine);
-    return;
-  }
-
-  // if we are under our max lines
-  if(lines.length==maxLinesPerScreen){
-    lines.splice(0,1);
-  }
-
-  lines.push(line);
-
-}
-
-function updateConsole(){
-
-  var content = '';
-  lines.forEach(function(value, index){
-
-    // if the async call added a cursor, and it is not the last line, delete
-    // the cursor
-
-    if(index != lines.length-1 && value[value.length-1] == "|"){
-      lines[lines.length-1] = line.substring(0, line.length - 1);
+        });
+        return;
     }
 
-    content += value + "\r\n";
+    if (line.length > maxCharsPerLine) {
+        const words = line.split(" ");
+        let tempLine = "";
 
-  });
+        words.forEach(function (word) {
+            if ((tempLine + word).length + 1 < maxCharsPerLine) {
+                tempLine += word + " ";
+            } else {
+                addNewLine(tempLine);
+                tempLine = word + " ";
+            }
+        });
 
-  var console = document.getElementById('console');
-  console.focus();
-  console.innerHTML = content;
+        addNewLine(tempLine);
+        return;
+    }
+
+    // if we are under our max lines
+    if (lines.length === maxLinesPerScreen) {
+        lines.splice(0, 1);
+    }
+
+    lines.push(line);
 
 }
 
-function runCommand(command){
+function updateConsole() {
 
-  // this function is defined in parse.js
-  var tokens = parse(command);
+    let content = '';
+    lines.forEach(function (value, index) {
 
-  // this function is defined in command.js
-  doCommand(tokens);
+        // if the async call added a cursor, and it is not the last line, delete
+        // the cursor
+
+        if (index !== lines.length - 1 && value[value.length - 1] === "|") {
+            lines[lines.length - 1] = value.substring(0, value.length - 1);
+        }
+
+        content += value + "\r\n";
+
+    });
+
+    const console = document.getElementById('console');
+    console.focus();
+    console.innerHTML = content;
+
+}
+
+function runCommand(command) {
+
+    // this function is defined in parse.js
+    const tokens = parse(command);
+
+    // this function is defined in command.js
+    doCommand(tokens);
 
 }
 
 // add a blinking cursor effect
-function blink(){
-  var lastLine = lines[lines.length-1];
-  // add a cursor, or delete it
-  if(lastLine[lastLine.length-1] != "|"){
-    lines[lines.length-1] += "|";
-  }
-  else{
-    lines[lines.length-1] = lastLine.substring(0, lastLine.length - 1);
-  }
+function blink() {
+    const lastLine = lines[lines.length - 1];
+    // add a cursor, or delete it
+    if (lastLine[lastLine.length - 1] !== "|") {
+        lines[lines.length - 1] += "|";
+    } else {
+        lines[lines.length - 1] = lastLine.substring(0, lastLine.length - 1);
+    }
 
-  updateConsole();
+    updateConsole();
 }
 
-function selectConsole(){
-  $('#console').focus();
+function selectConsole() {
+    document.getElementById('console').focus();
 }
--- parse.js
+++ parse.js
@@ -1,55 +1,37 @@
-function parse(command){
+function parse(command) {
 
-  var values = command.split(" ");
+    const values = command.split(" ");
 
-  var tokens = [];
+    const tokens = [];
 
-  values.forEach(function(value){
+    values.forEach(function (value) {
 
-    value = value.toLowerCase();
+        value = value.toLowerCase();
 
-    if(value == 'go'){
-      tokens.push({type:'GO'});
-    }
+        if (value === 'go') {
+            tokens.push({type: 'GO'});
+        } else if (value === 'info') {
+            tokens.push({type: 'INFO'});
+        } else if (value === 'use' || value === 'read' || value === 'attack' || value === 'examine' || value === 'eat') {
+            tokens.push({type: 'USE', value: value});
+        } else if (value === 'with' || value === 'on' || value === 'for') {
+            tokens.push({type: 'WITH'});
+        } else if (value === 'help' || value === "?") {
+            tokens.push({type: 'HELP'});
+        } else if (value === 'take' || value === "grab") {
+            tokens.push({type: 'TAKE'});
+        } else if (value === 'inventory' || value === 'inv') {
+            tokens.push({type: 'INVENTORY'});
+        } else if (value === 'speak' || value === 'say' || value === 'talk' || value === 'yell' || value === 'scream' || value === 'whisper') {
+            tokens.push({type: 'SAY', value: value});
+        } else if (value === "") {
+            // do nothing for blank values
+        } else {
+            tokens.push({type: 'VALUE', value: value});
+        }
 
-    else if(value == 'info'){
-      tokens.push({type:'INFO'});
-    }
+    });
 
-    else if(value == 'use' || value == 'read' || value == 'attack' || value == 'examine' || value == 'eat'){
-      tokens.push({type:'USE',value:value});
-    }
-
-    else if(value == 'with' || value == 'on' || value == 'for'){
-      tokens.push({type:'WITH'});
-    }
-
-    else if(value == 'help' || value == "?"){
-      tokens.push({type:'HELP'});
-    }
-
-    else if(value == 'take' || value == "grab"){
-      tokens.push({type:'TAKE'});
-    }
-
-    else if(value == 'inventory' || value == 'inv'){
-      tokens.push({type:'INVENTORY'});
-    }
-
-    else if(value == 'speak' || value == 'say' || value == 'talk' || value == 'yell' || value == 'scream' || value == 'whisper'){
-      tokens.push({type:'SAY',value:value});
-    }
-
-    else if(value == ""){
-      // do nothing for blank values
-    }
-
-    else{
-      tokens.push({type:'VALUE',value:value});
-    }
-
-  });
-
-  return tokens;
+    return tokens;
 
 }
